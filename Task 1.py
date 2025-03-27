722f5d37f577550b416af602a7fe042b5f12395c
Aarya Taksha.A <takshapower@gmail.com>
2025-03-27 10:48:10 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,8 @@
+N1 = float(input("Enter the first number :"))
+N2 = float(input("Enter the second number :"))
+
+
+print("The sum of these two numbers is :", N1 + N2)
+print("The product of these two numbers is :", N1 * N2)
+print("The quotient of these two numbers is :", N1 / N2)
+print("The difference of these two numbers is :", N1 - N2)
---
+++ Task 2.py
@@ -0,0 +1,3 @@
+FN = str(input("Enter your First Name :"))
+LN = str(input("Enter your Last Name :")) 
+print("Hello,", FN,LN,"welcome to this amazing platform!")
